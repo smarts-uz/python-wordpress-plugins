a2c03a6a31c44df12c23a188e8896b98b1d971a0
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-04-14 18:10:11 -0700

    parse wordpress plugins

---
+++ parse_plugins.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+import urllib.request as hyperlink
+import os
+
+from url_txt import create_url
+
+
+def plugins_parse():
+    link = hyperlink.urlopen('http://plugins.svn.wordpress.org/')
+    wordPressSoup = BeautifulSoup(link, 'html.parser')
+    plugins_lists = wordPressSoup.find('ul')
+    plugins = plugins_lists.find_all('li')
+    for plugin in plugins:
+        plugin_name = plugin.get_text(strip=True)
+        plugin_folder_name = f'All/{plugin_name.replace("/", "").replace("-"," ").title()}'
+        if os.path.exists(plugin_folder_name):
+            print(f'{plugin_folder_name} folder already exists')
+        else:
+            os.mkdir(plugin_folder_name)
+            print(f'{plugin_folder_name} folder created')
+            create_url(path=plugin_folder_name,name=plugin_name)
+
+
+plugins_parse()
---
+++ url_txt.py
@@ -0,0 +1,16 @@
+def create_url(path,name):
+    try:
+        with open(f'{path}/{name.replace("/","")}.url',mode='w',encoding='utf-8') as file:
+            a = '{000214A0-0000-0000-C000-000000000046}'
+            str = f"""[{a}]
+Prop3=19,11
+[InternetShortcut]
+IDList=
+URL=https://wordpress.org/plugins/{name}
+IconIndex=13
+HotKey=0
+IconFile=C:\\Windows\\System32\\SHELL32.dll"""
+            file.write(str)
+        print(f'Created url file:{path}/{name.replace("/","")}.url')
+    except Exception as e:
+        print(e)
