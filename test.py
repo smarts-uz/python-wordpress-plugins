d1bcedee491060400d858f262c26b3c4b5fa33da
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-04-17 14:15:14 +0500

    del

--- test.py
+++
@@ -1,8 +0,0 @@
-# import requests
-#
-# url  = 'https://ps.w.org/012-ps-multi-languages/trunk/screenshot-1.png'
-# # url  = 'https://ps.w.org/012-ps-multi-languages/trunk/'
-# import urllib.request as hyperlink
-# response = hyperlink.urlopen(url)
-#
-# print(response.read())
