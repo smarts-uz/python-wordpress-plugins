3084cd1a38d089ccbb73c093543eeb7cdb1af16e
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-04-22 13:24:00 +0500

    create txt file for unused plugins

--- Parsing/plugin_description.py
+++ Parsing/plugin_description.py
@@ -1,4 +1,11 @@
-
+import sys
+sys.dont_write_bytecode = True
+# Django specific settings
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_orm.settings')
+import django
+django.setup()
+from django_orm.db.models import Plugin
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
@@ -22,9 +29,9 @@ def plugin_desc():
                     url = re.findall(r'https://wordpress.org/plugins.+', data)[0]
                 response = requests.get(url, timeout=60)
                 soup = BeautifulSoup(response.text, 'html.parser')
-                description = soup.find('div', id="tab-description",class_='plugin-description section')
+                description_1 = soup.find('div', id="tab-description",class_='plugin-description section')
                 try:
-                    description = description.find('div', class_='plugin-notice notice notice-error notice-alt').get_text(strip=True)
+                    description = description_1.find('div', class_='plugin-notice notice notice-error notice-alt').get_text(strip=True)
                     if description !=  None:
                         description_path = f'{description.replace("  ", " ")}.txt'
                         plugin_desc_path = os.path.join(plugin_path, description_path)
@@ -41,4 +48,33 @@ def plugin_desc():
 
 
 
-plugin_desc()
+
+
+def unused_plugin():
+    plugins = Plugin.objects.filter(unused=False)
+    for plugin in plugins:
+        plugin_path = plugin.folder_path
+        html_file_path = os.path.join(plugin.folder_path, plugin.html)
+        with open(html_file_path, 'rb') as f:
+            html_body = f.read()
+        soup = BeautifulSoup(html_body, 'html.parser')
+        try:
+            description_1 = soup.find('div', id="tab-description", class_='plugin-description section')
+            description = description_1.find('div', class_='plugin-notice notice notice-error notice-alt').get_text(
+                strip=True)
+            if description != None:
+                description_path = f'{description.replace("  ", " ")}.txt'
+                plugin_desc_path = os.path.join(plugin_path, description_path)
+                if not os.path.isfile(plugin_desc_path):
+                    with open(plugin_desc_path, 'w') as f:
+                        f.write(description)
+                    print(f'{plugin_desc_path} has been created txt file')
+                else:
+                    print(f'{plugin_desc_path} already exists')
+            plugin.unused = True
+            plugin.save()
+        except:
+             pass
+
+
+unused_plugin()
