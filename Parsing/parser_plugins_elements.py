681b01f599b130a751c303580a591f23a73dd632
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-04-22 13:11:37 +0500

    parsing wordpress plugins informations #2

--- Parsing/parser_plugins_elements.py
+++ Parsing/parser_plugins_elements.py
@@ -1,5 +1,11 @@
-import time
-
+import sys
+sys.dont_write_bytecode = True
+# Django specific settings
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_orm.settings')
+import django
+django.setup()
+from django_orm.db.models import Plugin
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
@@ -68,9 +74,48 @@ def plugins_elements():
 
 
 
+def plugins_elements_v2():
+    plugins = Plugin.objects.filter(elements=False)
+    for pl in plugins:
+        plugin_path = pl.folder_path
+        html_file_path = os.path.join(pl.folder_path, pl.html)
+        with open(html_file_path, 'rb') as f:
+            html_body = f.read()
+        soup = BeautifulSoup(html_body, 'html.parser')
+        try:
+            plugins_info_class = soup.find('div', class_='widget plugin-meta').find('ul').find_all('li')
+            for plugin in plugins_info_class:
+                if plugin('div', class_='tags'):
+                    tags = plugin.find('div', class_='tags').find_all('a')
+                    for tag in tags:
+                        tag_path = os.path.join(plugin_path, f'#{tag.text}.txt')
+                        if not os.path.isfile(tag_path):
+                            with open(tag_path, 'w') as f:
+                                f.write(tag.text)
+                                print(f'Created txt file {tag_path}')
+                        else:
+                            print(f'File {tag_path} already exists')
+                else:
+                    plugin_text = plugin.text
+                    if 'Advanced View' not in plugin_text:
+                        plugin_text = plugin.get_text(' ', strip=True)
+                        unsupchar = ["*", '"', "/", "\\", "<", ">", ":", "|", "?"]
+                        for char in unsupchar:
+                            plugin_text = plugin_text.replace(char, " ")
+                        plugin_text = plugin_text.replace('  ', ' ')
+                        plugin_text_path = os.path.join(plugin_path, f'{plugin_text}.txt')
+                        if not os.path.isfile(plugin_text_path):
+                            with open(plugin_text_path, 'w') as f:
+                                f.write(plugin_text)
+                                print(f'Created txt file {plugin_text_path}')
+                        else:
+                            print(f'File {plugin_text_path} already exists')
+            pl.elements = True
+            pl.save()
+        except:
+            pass
 
 
 
 
-
-plugins_elements()
+plugins_elements_v2()
